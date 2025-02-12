572d7f4262aa3cbb484d97e7d09644f7cb8ac7f5
Hasithchodisetty <hasithchodisetty@gmail.com>
2025-02-12 21:00:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('timetableBtn').addEventListener('click', function() {
+    const timetableSection = document.getElementById('timetableSection');
+    timetableSection.classList.toggle('d-none');
+});
